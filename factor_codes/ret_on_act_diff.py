92d35e70180abdcaba34d80dd49ccc2543231f30
tianyixin <3634579832@qq.com>
2025-10-14 16:28:26 +0800

    1014

--- factor_codes/ret_on_act_diff.py
+++ factor_codes/ret_on_act_diff.py
@@ -1,3 +1,5 @@
+
+
 # diff=主动买-主动卖的量,选取diff最高/最低的10%的区间，表示投资者对该只股票异常热情/冷淡
 # (也可以diff主动买或主动卖，看情绪变化激增或骤减的情况，这些区间挑选出来)
 # order buy和实际成交之间的关系
@@ -27,19 +29,26 @@ def cal(date):
     df['min_ret']=df['close']/df['open']-1
     df.replace([np.inf, -np.inf], np.nan, inplace=True)
     df.sort_values(['TICKER','min'],inplace=True)
-    # df['act_diff']=df['active_buy_volume']-df['active_sell_volume'] #todo:这是第一版
-    df['act_diff']=df.groupby('TICKER')['active_buy_volume'].diff() #todo: 这是第三版
+    df['buy_diff_sell']=df['active_buy_volume']-df['active_sell_volume']
+    df['act_diff_self']=df.groupby('TICKER')['active_buy_volume'].diff()
     tmp1=df.groupby('TICKER')['min_ret'].mean()
-    # 最高排名10%，20%
-    df.sort_values(['TICKER','act_diff'],ascending=[True,False],inplace=True)
-    res1=help(df,100,tmp1,'retOnVol_top_24') #24 #todo:top_num的变化是第二版
-    res2=help(df,150,tmp1,'retOnVol_top_47') #47
-    # 最低排名10%，20%
-    df.sort_values(['TICKER','act_diff'],inplace=True)
-    res3=help(df,100,tmp1,'retOnVol_bottom_24')
-    res4=help(df,150,tmp1,'retOnVol_bottom_47')
-    tmp=res1.merge(res2,on=['TICKER'],how='inner').merge(res3,on=['TICKER'],
-                                            how='inner').merge(res4,on=['TICKER'],how='inner')
+    # ----------------------
+    # 根据buy_diff_sell排序
+    # 最高排名10%
+    df.sort_values(['TICKER','buy_diff_sell'],ascending=[True,False],inplace=True)
+    res1=help(df,24,tmp1,'retOnNetAct_top_24')
+    # 最低排名10%
+    # df.sort_values(['TICKER','buy_diff_sell'],inplace=True)
+    # res2=help(df,24,tmp1,'retOnNetAct_bottom_24')
+    # ----------------------
+    # 根据act_diff_self排序
+    df.sort_values(['TICKER','act_diff_self'],ascending=[True,False],inplace=True)
+    res3=help(df,24,tmp1,'retOnBuyDiff_top_24')
+    # df.sort_values(['TICKER', 'act_diff_self'], inplace=True)
+    # res4 = help(df, 24, tmp1, 'retOnBuyDiff_bottom_24')
+    # tmp=res1.merge(res2,on=['TICKER'],how='inner').merge(res3,on=['TICKER'],
+    #                                         how='inner').merge(res4,on=['TICKER'],how='inner')
+    tmp=res1.merge(res3,on=['TICKER'],how='inner')
     tmp['DATE']=date
     tmp.replace([np.inf, -np.inf], np.nan, inplace=True)
     return tmp#[['DATE','TICKER','retOnVol_top_24']]
@@ -62,16 +71,19 @@ def run(start,end):
     res=pd.concat(res).reset_index(drop=True)
     res.sort_values(['TICKER', 'DATE'], inplace=True)
     res.replace([np.inf, -np.inf], np.nan, inplace=True)
-    res = process_na_stock(res, 'retOnVol_top_24')
-    res['retOnVol_top_24_roll20'] = res.groupby('TICKER')['retOnVol_top_24'].rolling(20, 5).mean().values
-    res['retOnVol_top_47_roll20'] = res.groupby('TICKER')['retOnVol_top_47'].rolling(20, 5).mean().values
-    res['retOnVol_bottom_24_roll20'] = res.groupby('TICKER')['retOnVol_bottom_24'].rolling(20, 5).mean().values
-    res['retOnVol_bottom_47_roll20'] = res.groupby('TICKER')['retOnVol_bottom_47'].rolling(20, 5).mean().values
+    res = process_na_stock(res, 'retOnNetAct_top_24')
+    res['retOnNetAct_top_24_roll20'] = res.groupby('TICKER')['retOnNetAct_top_24'].rolling(20, 5).mean().values
+    # res['retOnNetAct_bottom_24_roll20'] = res.groupby('TICKER')['retOnNetAct_bottom_24'].rolling(20, 5).mean().values
+    res['retOnBuyDiff_top_24_roll20'] = res.groupby('TICKER')['retOnBuyDiff_top_24'].rolling(20, 5).mean().values
+    # res['retOnBuyDiff_bottom_24_roll20'] = res.groupby('TICKER')['retOnBuyDiff_bottom_24'].rolling(20, 5).mean().values
     res.replace([np.inf, -np.inf], np.nan, inplace=True)
-    res=res[['DATE','TICKER','retOnVol_top_24_roll20','retOnVol_top_47_roll20','retOnVol_bottom_24_roll20','retOnVol_bottom_47_roll20']]
+    # res=res[['DATE','TICKER','retOnNetAct_top_24_roll20','retOnNetAct_bottom_24_roll20','retOnBuyDiff_top_24_roll20',
+    #          'retOnBuyDiff_bottom_24_roll20']]
+    res=res[['DATE','TICKER','retOnNetAct_top_24_roll20','retOnBuyDiff_top_24_roll20']]
     return [res]
 def update(today):
-    update_muli('retOnVol_top_24_roll20.feather',today,run)
+    update_muli('retOnNetAct_top_24_roll20.feather',today,run)
+
 
 def update_muli(filename,today,run,num=-50):
     if os.path.exists(os.path.join(DataPath.save_path_update,filename)):
@@ -94,7 +106,7 @@ def update_muli(filename,today,run,num=-50):
                     old=pd.concat([old,tmp]).reset_index(drop=True).drop_duplicates()
                     print(old)
                     feather.write_dataframe(old,os.path.join(DataPath.save_path_update,col+'.feather'))
-                    # feather.write_dataframe(old,os.path.join(DataPath.factor_out_path,col+'.feather'))
+                    feather.write_dataframe(old,os.path.join(DataPath.factor_out_path,col+'.feather'))
                 else:
                     print(test[~np.isclose(test.iloc[:,2],test.iloc[:,3])])
                     # tt=test[~np.isclose(test.iloc[:, 2], test.iloc[:, 3])]
@@ -103,16 +115,16 @@ def update_muli(filename,today,run,num=-50):
                     exit()
     else:
         print('因子生成中')
-        res=run(start='20211201',end='20250801')
-        # res=run(start='20200101',end='20250822')
+        # res=run(start='20211201',end='20250801')
+        res=run(start='20200101',end='20221231')
         # res=run(start='20250828',end='20250829')
         for df in res:
             for col in df.columns[2:]:
                 tmp=df[['DATE','TICKER',col]]
                 print(tmp)
-                # feather.write_dataframe(tmp,os.path.join(DataPath.save_path_old,col+'.feather'))
-                # feather.write_dataframe(tmp,os.path.join(DataPath.save_path_update,col+'.feather'))
-                feather.write_dataframe(tmp,os.path.join(r'C:\Users\admin\Desktop\test',col+'.feather'))
+                feather.write_dataframe(tmp,os.path.join(DataPath.save_path_old,col+'.feather'))
+                feather.write_dataframe(tmp,os.path.join(DataPath.save_path_update,col+'.feather'))
+                # feather.write_dataframe(tmp,os.path.join(r'C:\Users\admin\Desktop\test',col+'.feather'))
 
 if __name__=='__main__':
     # run('20250102','20250106')
